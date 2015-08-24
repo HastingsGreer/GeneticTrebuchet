5e8e23f076ae5728eb9cd3f65c4331249c25bfdc
Hastings Greer <hastings.greer@gmail.com>
2015-08-24 17:44:42 -0400

    added logging of successful trebuchets:x

--- trebuchetmaximizer2.py
+++ trebuchetmaximizer2.py
@@ -4,6 +4,8 @@ import genetic
 import treblink
 import numpy as np
 
+f = open("output.data", 'a', buffering = 0)
+
 class TrebMax(genetic.Individual):
     optimization = genetic.MAXIMIZE
     def __init__(self, chromosome=None):
@@ -14,7 +16,10 @@ class TrebMax(genetic.Individual):
         return treblink.LinkTrebuchet([treblink.TrebLink(random.random()*10, 1+random.random()*9, []).mutate() for a in range(5)]) 
     def evaluate(self, optimum=None):
         print self.chromosome
-        self.score = self.chromosome.evaluate()        
+        self.score = self.chromosome.evaluate()
+        if self.score > .0001:
+            f.write("\n" + self.chromosome + '\n' + str(self.score)
+                     
     def mutate(self, gene):
         self.chromosome = self.chromosome.mutate()
     def crossover(self, other):
